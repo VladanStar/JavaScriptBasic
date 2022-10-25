fcd51879c5245813178f8edcf069e5529d1e9ba2
unknown <vladancupric@gmail.com>
2022-10-25 21:34:53 +0200

    Add

---
+++ 02_Unique_Values/main.js
@@ -0,0 +1,27 @@
+// Unique Values
+const menu = [
+    {
+      name: 'pancakes',
+      category: 'breakfast',
+    },
+    {
+      name: 'burger',
+      category: 'lunch',
+    },
+    {
+      name: 'steak',
+      category: 'dinner',
+    },
+    {
+      name: 'bacon',
+      category: 'breakfast',
+    },
+    {
+      name: 'eggs',
+      category: 'breakfast',
+    },
+    {
+      name: 'pasta',
+      category: 'dinner',
+    },
+  ];
