0306a86725dda9965daed546dfc5d5f1a2b4745b
VladanStar <vladancupric@gmail.com>
2022-10-27 08:51:31 +0200

    add

--- 03_Dinamyc_Object_Keys/main.js
+++ 03_Dinamyc_Object_Keys/main.js
@@ -5,3 +5,14 @@ console.log(person.name);
 person.age=25;
 {name:'John'}
 console.log(person); 
+
+const items = {
+    'featured-items':['item1','item2']
+}
+console.log(items['featured-items']);
+console.log(person['name'])
+let appState = 'loading'
+const app = {
+    [appState]:true
+}
+console.log(app)
