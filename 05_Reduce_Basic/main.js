6513da4c6b10481302b067663890c0aed00bb624
VladanStar <vladancupric@gmail.com>
2022-10-31 10:34:17 +0100

    Add

--- 05_Reduce_Basic/main.js
+++ 05_Reduce_Basic/main.js
@@ -2,36 +2,36 @@ const staff = [
   {
     name: "Bob",
     age: 20,
-        position: "developer",
-    salary:1020,
+    position: "developer",
+    salary: 1020,
   },
   {
     name: "Peter",
     age: 25,
-      position: "desinger",
-      salary: 700,
+    position: "desinger",
+    salary: 700,
   },
   {
     name: "Susy",
     age: 30,
-      position: "the boss",
-      salary: 1000,
+    position: "the boss",
+    salary: 1000,
   },
   {
     name: "Anna",
     age: 35,
-      position: "intern",
-      salary: 4100,
+    position: "intern",
+    salary: 4100,
   },
   {
     name: "Vladan",
     age: 49,
-      position: "developer",
-      salary: 1100,
+    position: "developer",
+    salary: 1100,
   },
 ];
 const dailytotal = staff.reduce((total, person) => {
-    total += person.salary;
-    return total
-}, 0)
-console.log(dailytotal)
+  total += person.salary;
+  return total;
+}, 0);
+console.log(dailytotal);
