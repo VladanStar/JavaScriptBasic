e1103921d33c0d45021e236c6710a01882c890e2
VladanStar <vladancupric@gmail.com>
2022-10-31 08:40:22 +0100

    Add

---
+++ 04_FilterAndFind/main.js
@@ -0,0 +1,23 @@
+const people = [
+    { name:"Bob", age:20, position:"developer"
+    },
+    { name:"Peter", age:25, position:"desinger"
+    },
+    { name:"Susy", age:30, position:"the boss"
+    },
+    {
+        name: "Anna", age:35, position: "intern"
+    },
+    {
+        name: "Vladan", age: 49, position: "developer"
+    },
+
+
+]
+
+const youngPeople = people.filter((person) => {
+    if(person.age <30){
+        return person;
+    }
+})
+console.log(youngPeople)
