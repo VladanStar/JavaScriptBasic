c1c43c3d65befb70df186b1554752f5dfe4fca06
VladanStar <vladancupric@gmail.com>
2022-10-31 10:22:43 +0100

    Add

---
+++ 05_Reduce_Basic/main.js
@@ -0,0 +1,35 @@
+const staff = [
+  {
+    name: "Bob",
+    age: 20,
+        position: "developer",
+    salary:1020,
+  },
+  {
+    name: "Peter",
+    age: 25,
+      position: "desinger",
+      salary: 700,
+  },
+  {
+    name: "Susy",
+    age: 30,
+      position: "the boss",
+      salary: 1000,
+  },
+  {
+    name: "Anna",
+    age: 35,
+      position: "intern",
+      salary: 4100,
+  },
+  {
+    name: "Vladan",
+    age: 49,
+      position: "developer",
+      salary: 1100,
+  },
+];
+const dailytotal = staff.reduce(() => {
+    
+},0)
