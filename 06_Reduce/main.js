25ea74e16dfa27d67bc8f2592b5d22539e4a61b4
VladanStar <vladancupric@gmail.com>
2022-11-10 08:01:15 +0100

    Add

---
+++ 06_Reduce/main.js
@@ -0,0 +1,25 @@
+
+
+const cart =[
+    {
+        title:'Samsung Galaxy S7',
+        price:599.99,
+        amount:1,
+    },
+    {
+        title:'Samsung Galaxy S7',
+        price:599.99,
+        amount:1,
+    },
+    {
+        title:'Samsung Galaxy S7',
+        price:599.99,
+        amount:1,
+    },
+    {
+        title:'Samsung Galaxy S7',
+        price:599.99,
+        amount:1,
+    },
+]
+const url = 'https://api.github.com/users/john-smigla/repos?per_page=100'
