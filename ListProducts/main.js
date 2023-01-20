ac73dfccaef1f125f25d121f2ca7ee962fdba798
VladanStar <vladancupric@gmail.com>
2023-01-20 13:45:19 +0100

    Add

--- ListProducts/main.js
+++ ListProducts/main.js
@@ -1,21 +1,38 @@
-// fetch(
-//   "https://services.odata.org/V3/Northwind/Northwind.svc/Customers"
-// ).then((data) => {
-//     return data.json()
-// }). then((objectData)=>{
-//     console.log(objectData)
-// })
+fetch(
+  "https://services.odata.org/V3/Northwind/Northwind.svc/Customers"
+).then((data) => {
+    return data.json()
+}). then((objectData)=>{
+    console.log(objectData[0].title)
+    let tableData=""
+    objectData.map((values)=>{
+        tableData =<h1>${values.title}</h1>
+    })
+})
 
-fetch('https://services.odata.org/V3/Northwind/Northwind.svc/Order_Details', {
-  method: 'post',
-  headers: {
-    "Content-type": "application/json; charset=UTF-8"
-  },
-//   body:JSON.stringify(options)
-}).then(function(res){
-  return res.json(); //error here
-}).then(function(data){
-  console.log(data[0].title);
-}).catch((error) => {
-  console.log(error);
-});
+// fetch('https://services.odata.org/V3/Northwind/Northwind.svc/Order_Details', {
+//   method: 'post',
+//   headers: {
+//     "Content-type": "application/json; charset=UTF-8"
+//   },
+// //   body:JSON.stringify(options)
+// }).then(function(res){
+//   return res.json(); //error here
+// }).then(function(data){
+//   console.log(data[0].title);
+// }).catch((error) => {
+//   console.log(error);
+// });
+// var myVar = {"id" : 1};
+// fetch("https://services.odata.org/V3/Northwind/Northwind.svc/Products?$format=json", {
+//         method: "POST",
+    
+//         headers: {
+//             "Content-Type": "text/plain"
+//         },
+//         body: JSON.stringify(myVar)
+//     }).then(function(response) {
+//         return response.text();
+//     }).then(function(muutuja){
+       
+//     });
