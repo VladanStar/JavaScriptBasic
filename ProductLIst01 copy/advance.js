47110223b229b7a7ffb150aae4d16987652ac7d5
VladanStar <vladancupric@gmail.com>
2023-01-22 23:25:06 +0100

    Add

--- ProductLIst01 copy/advance.js
+++ ProductLIst01 copy/advance.js
@@ -35,40 +35,24 @@ $(function () {
         console.log(startDate, endDate);
         var endpoint =
           "https://services.odata.org/v4/Northwind/Northwind.svc/Orders/";
-          let grid = document.querySelector(".products");
+
         fetch(endpoint)
           .then((res) => res.json())
           .then((data) => {
             console.log(data.value);
             let dataA = data.value;
 
-            searchDate(grid,data);
+            searchDate(data);
           });
-        function searchDate(appendIn,dataA) {
+        function searchDate(dataA) {
           var dataA = dataA.value;
-          console.log(dataA)
+          console.log(dataA);
           var filteredDates = _.filter(dataA, function (dataA) {
-            return (dataA.OrderDate > startDate && dataA.OrderDate < endDate);
+            return dataA.OrderDate > startDate && dataA.OrderDate < endDate;
           });
-          let div = document.createElement("div");
-          div.className = "item justify-self-center";
           console.log(filteredDates);
-         let name = dataA.ShipName;
-          console.log(name)
-          div.innerHTML=``;
-          div.innerHTML = `
-            <img src="https://www.slikomania.rs/fotky6509/fotos/CWFFL036.jpg" class="bg-cover img">
-            <div class="text-center py-3 font-poppins">
-                <h3 class="text-lg title name"> Name: ${name}</h3>
-                <h3 class="text-lg"> CustomerID${dataA.CustomerID}</h3>
-                <a href="$" class="block"><span class="text-sm text-red-400"> Order ID: ${dataA.OrderID}</span></a>
-                <span class="block py-3"><span class="text-md"> OrderDate: ${dataA.OrderDate}</span></span>
-                <span class="block py-3"> <span class="text-md"> RequiredDate: ${dataA.ShippedDate}</span></span>
-                <button class="border-2 px-8 py-1 bg-yellow-400 border rounded-md">Bay Now</button>
-            </div>
-            `;
-        
-          appendIn.appendChild(div);
+          let name = dataA.ShipName;
+          console.log(name);
         }
 
         $('input[name="datefilter"]').on(
