21370e24e5d5a083d383b5bb153639127c90e8e0
VladanStar <vladancupric@gmail.com>
2023-01-23 00:41:34 +0100

    Add

--- ProductLIst01 copy/advance.js
+++ ProductLIst01 copy/advance.js
@@ -49,27 +49,27 @@ $(function () {
 
           }
         }
-        let filterInput = document.getElementById("filter");
-        let grid = document.querySelector(".products");
+         let filterInput = document.getElementById("filter");
+         let grid = document.querySelector(".products");
 
-        // filterInput.addEventListener("click", filterSearch);
-        
-        // callback function
-          function filterSearch() {
-          let filterValue = filterInput.value;
-         // console.log(filterValue);
-          let item = grid.querySelectorAll('.item');
-        //console.log(item)
-          for (let i = 0; i < item.length; i++) {
-            let span = item[i].querySelector('.name');
-           // console.log(span)
-            if (span.innerHTML.indexOf(filterValue) > -1) {
-              item[i].style.display = "initial";
-            } else {
-              item[i].style.display = "none";
+            // filterInput.addEventListener("click", filterSearch);
+            
+            // callback function
+              function filterSearch() {
+              let filterValue = filterInput.value;
+             // console.log(filterValue);
+              let item = grid.querySelectorAll('.item');
+            //console.log(item)
+              for (let i = 0; i < item.length; i++) {
+                let span = item[i].querySelector('.name');
+               // console.log(span)
+                if (span.innerHTML.indexOf(filterValue) > -1) {
+                  item[i].style.display = "initial";
+                } else {
+                  item[i].style.display = "none";
+                }
+              }
             }
-          }
-        }
 
         $('input[name="datefilter"]').on(
           "cancel.daterangepicker",
