d68d4a5e01fbac5c4983d51439ce259d49ac3bee
VladanStar <vladancupric@gmail.com>
2023-01-22 15:38:34 +0100

    Add

--- ProductLIst01/advance.js
+++ ProductLIst01/advance.js
@@ -1,7 +1,32 @@
 
- const values = ["Beograd", "Cairo", "Casablanca","Warshava", "Osaka", "Caracas"]
+
+
+const values = ["Beograd", "Cairo", "Casablanca","Warshava", "Osaka", "Caracas"];
+
+function filterProducts(){
+
+    // remove all the childs from the current element
+    while(grid.childNodes.length > 1){
+        grid.removeChild(grid.lastChild)
+    }
+
+    fetch('./database/store.json')
+    .then(res => res.json())
+    .then(json =>{
+
+        let filterValue = filterInput.value.toUpperCase();
+        let filterData = match(json, 'title', filterValue)
+
+        for (const value of filterData){
+            addElement(grid, value)
+        }
+
+    });
+
+}
+
 //match
-const match = (values,input) =>{
+const match = (values, filterby,input) =>{
 
     const p = Array.from(input).reduce((a,v,i)=>`${a}[^${input.substr(i)}]*?${v}`, '');
     const re= RegExp(p);
