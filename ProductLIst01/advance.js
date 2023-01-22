dcd451a14a65eb302789ad6479eba2a05c52314b
VladanStar <vladancupric@gmail.com>
2023-01-22 15:35:17 +0100

    Add

--- ProductLIst01/advance.js
+++ ProductLIst01/advance.js
@@ -1,11 +1,11 @@
 
- const values = ["Bg", "Cr", "RT","WR", "Osaka"]
+ const values = ["Beograd", "Cairo", "Casablanca","Warshava", "Osaka", "Caracas"]
 //match
 const match = (values,input) =>{
 
-    const p = Array.from(input).reduce((a,v,i)=>`${a}[^$(input.substring(i)}]*?);
+    const p = Array.from(input).reduce((a,v,i)=>`${a}[^${input.substr(i)}]*?${v}`, '');
     const re= RegExp(p);
-    return values.filter(v => v.match(re))
-    console.log(p)
+    return values.filter(v => v[filterby].toUpperCase().match(re))
+   // console.log(p)
 }
-console.log(match(values, "C"));
+ console.log(match(values, "CA"));
