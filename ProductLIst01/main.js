bab456f72627991216cd9bd103e9283c23c1ec1c
VladanStar <vladancupric@gmail.com>
2023-01-22 11:50:31 +0100

    Add

--- ProductLIst01/main.js
+++ ProductLIst01/main.js
@@ -10,25 +10,46 @@ fetch("https://services.odata.org/v4/Northwind/Northwind.svc/Products/")
     }
   });
 
+//add event listener
+
+let filterInput = document.getElementById("filterInput");
+
+filterInput.addEventListener("keyup", filterProducts);
+
+// callback function
+function filterProducts() {
+  let filterValue = filterInput.value.toUpperCase();
+  console.log(filterValue);
+  let item = grid.querySelectorAll('.item');
+console.log(item)
+  for (let i = 0; i < item.length; i++) {
+    let span = item[i].querySelector('.name');
+    console.log(span)
+    if (span.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+      item[i].style.display = "initial";
+    } else {
+      item[i].style.display = "none";
+    }
+  }
+}
 function addElement(appendIn, value) {
   console.log(value);
   let div = document.createElement("div");
   div.className = "item justify-self-center";
 
-//   let { ProductID, ProductName, CategoryID, UnitPrice } = value;
+  //   let { ProductID, ProductName, CategoryID, UnitPrice } = value;
   console.log(value);
-  let inner =""
-  inner += `
+ let name = value.ProductName;
+  div.innerHTML = `
     <img src="https://www.slikomania.rs/fotky6509/fotos/CWFFL036.jpg" class="bg-cover img">
     <div class="text-center py-3 font-poppins">
-        <h3 class="text-lg title">${value.ProductName}</h3>
-        <h3 class="text-lg title">${value.ProductID}</h3>
+        <h3 class="text-lg title name">${name}</h3>
+        <h3 class="text-lg">${value.ProductID}</h3>
         <a href="$" class="block"><span class="text-sm text-red-400">${value.CategoryID}</span></a>
         <span class="block py-3">$ <span class="text-md">${value.UnitPrice}</span></span>
         <button class="border-2 px-8 py-1 bg-yellow-400 border rounded-md">Bay Now</button>
     </div>
     `;
-    div.innerHTML= inner;
 
   appendIn.appendChild(div);
 }
