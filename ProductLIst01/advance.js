143b595f1c71e5470bd1ca982201e8413bb0d973
VladanStar <vladancupric@gmail.com>
2023-01-22 14:18:00 +0100

    Add

---
+++ ProductLIst01/advance.js
@@ -0,0 +1,9 @@
+
+ const values = ["Bg", "Cr", "RT","WR", "Osaka"]
+//match
+const match = (input) =>{
+
+    const p = Array.from(input).reduce((a,v)=>v);
+    console.log(p)
+}
+console.log(match(values))
