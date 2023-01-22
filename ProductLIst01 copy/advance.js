8ad47bf3aa018e7140d9ed2c360eed116c496898
VladanStar <vladancupric@gmail.com>
2023-01-23 00:23:20 +0100

    Add

--- ProductLIst01 copy/advance.js
+++ ProductLIst01 copy/advance.js
@@ -31,6 +31,7 @@ $(function () {
             let dataA = data.value;
 
             searchDate(data);
+            filterSearch()
           });
         function searchDate(dataA) {
           var dataA = dataA.value;
@@ -42,6 +43,31 @@ $(function () {
           for (let i = 0; i < filteredDates.length; i++) {
             let name = filteredDates[i].CustomerID;
             console.log(name);
+
+           
+
+
+          }
+        }
+        let filterInput = document.getElementById("filter");
+        let grid = document.querySelector(".products");
+
+        // filterInput.addEventListener("click", filterSearch);
+        
+        // callback function
+          function filterSearch() {
+          let filterValue = filterInput.value;
+         // console.log(filterValue);
+          let item = grid.querySelectorAll('.item');
+        //console.log(item)
+          for (let i = 0; i < item.length; i++) {
+            let span = item[i].querySelector('.name');
+           // console.log(span)
+            if (span.innerHTML.indexOf(filterValue) > -1) {
+              item[i].style.display = "initial";
+            } else {
+              item[i].style.display = "none";
+            }
           }
         }
 
