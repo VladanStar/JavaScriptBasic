8122cbdfdc1595b8c6ba4455afd8cc5514a90a99
VladanStar <vladancupric@gmail.com>
2023-01-24 17:58:38 +0100

    Add

--- ProductLIst01 copy 2/orders.js
+++ ProductLIst01 copy 2/orders.js
@@ -1,14 +1,9 @@
-
-
-// let  btnE2 = document.querySelector('.btn2');
-// btnE2.addEventListener('click', 'order');
-
-
-
-
+//"https://services.odata.org/V3/Northwind/Northwind.svc/Customers?$format=json";
 document.getElementsByClassName("container")[0].style.display = "none";
 // import { userE } from "./login.js";
-fetch("https://services.odata.org/v4/Northwind/Northwind.svc/Orders/")
+fetch(
+  "https://services.odata.org/v4/Northwind/Northwind.svc/Orders?$format=json"
+)
   .then((response) => response.json())
   .then((data) => {
     console.log(data.value);
@@ -20,61 +15,12 @@ function addTable(value) {
   let tableData = "";
 
   tableData += `
-        <tr>
+        <tr class= "tada">
             <td> ${value.OrderID}</td>
             <td>${value.CustomerID}</td>
-            <td>${value.OrderDate}</td>
+            <td class="odate">${value.OrderDate.slice(0, 10)}</td>
             <td>${value.Freight}</td>
         </tr > `;
 
   document.getElementById("table_body").innerHTML += tableData;
 }
-$('input[name="datefilter"]').on(
-  "apply.daterangepicker",
-  function (ev, picker) {
-    $(this).val(
-      // 1996-07-04
-      picker.startDate.format("YYYY-MM-DD") +
-        " - " +
-        picker.endDate.format("YYYY-MM-DD")
-    );
-    var startDate = picker.startDate.format("YYYY-MM-DD");
-    var endDate = picker.endDate.format("YYYY-MM-DD");
-    if (startDate != "" && endDate != "") {
-      console.log(startDate, endDate);
-      var endpoint =
-        "https://services.odata.org/V3/Northwind/Northwind.svc/Orders?$format=json";
-
-      fetch(endpoint)
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data.value);
-          let dataA = data.value;
-
-          searchDate(dataA);
-         
-        });
-      function searchDate(dataA) {
-        var dataA = dataA.value;
-        console.log(dataA);
-        var filteredDates = _.filter(dataA, function (dataA) {
-          return dataA.OrderDate > startDate && dataA.OrderDate < endDate;
-        });
-        console.log(filteredDates);
-       
-  
-
-      
-
-      }
-
-      $('input[name="datefilter"]').on(
-        "cancel.daterangepicker",
-        function (ev, picker) {
-          $(this).val("");
-        }
-      );
-    }
-  }
-);
-
