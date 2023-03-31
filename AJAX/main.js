5e9947cc808bdc700dfa132774a8b2b473e0f15e
VladanStar <vladancupric@gmail.com>
2023-03-31 13:05:48 +0200

    Ass

--- AJAX/main.js
+++ AJAX/main.js
@@ -1,5 +1,5 @@
 
-fetch('./db.json')
+fetch('https://6426b703d24d7e0de4765ab5.mockapi.io/player')
   .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.error(error));
